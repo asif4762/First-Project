27b636809962725cbdda6e873144b9afa6c81e8f
test <test>
2024-11-19 23:52:00 +0600

    one prb

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,99 @@
+import { Schema , model } from 'mongoose';
+import { Guardian, localGuardian, Student, UserName } from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+        firstName: {
+            type: String,
+            required: true,
+        },
+        middleName:{
+            type: String,
+        },
+        lastName:{
+            type: String,
+            required: true
+        },
+})
+
+const guardianSchema = new Schema<Guardian>({
+        fatherName: {
+            type: String,
+            required: true
+        },
+        fatherContactNo: {
+            type: String,
+            required: true
+        },
+        fatherOccupation:{
+            type: String,
+            required: true
+        },
+        motherName: {
+            type: String,
+            required: true
+        },
+        motherContactNo: {
+            type: String,
+            required: true
+        },
+        motherOccupation:{
+            type: String,
+            required: true
+        },
+})
+
+const localGuardian = new Schema<localGuardian>({
+        name: {
+            type: String,
+            required: true
+        },
+        occupation: {
+            type: String,
+            required: true
+        },
+        contactNo: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+})
+
+const studentSchema = new Schema<Student>({
+    id: {type: String},
+    name: userNameSchema,
+    gender: ["female","male"],
+    dateOfBirth: {type: String},
+    email: {
+        type: String,
+        required: true
+    },
+    contactNo: {
+        type: String,
+        required: true
+    },
+    emergencyContactNo: {
+        type: String,
+        required: true
+    },
+    bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    presentAddress: {
+        type: String,
+        required: true
+    },
+    permanentAddress: {
+        type: String,
+        required: true
+    },
+    guardian: guardianSchema,
+    localGuardian: localGuardian,
+    profileImg: {
+        type: String,
+    },
+    isActive: ['active', 'inActive'],
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
+
